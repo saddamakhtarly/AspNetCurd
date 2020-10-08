dc94c02561c9b7fac29348eaf15be2d280837812
SaddamAkhtar <saddamakhtar.ly@gmail.com>
2020-10-08 15:34:26 +0500

    Add project files.

---
+++ AspNetCurd/Controllers/EmployeeController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AspNetCurd.Models;
+
+namespace AspNetCurd.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET: Employee
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult GetData()
+        {
+            using (DBModel db= new DBModel())
+            {
+                List<TblEmployee> empList = db.TblEmployees.ToList<TblEmployee>();
+                return Json(new { data = empList }, JsonRequestBehavior.AllowGet);
+            }
+        }
+        [HttpGet]
+        public ActionResult AddorEdit(int id=0)
+        {
+            return View(new TblEmployee());
+        }
+        [HttpPost]
+        public ActionResult AddorEdit(TblEmployee emp)
+        {
+            using (DBModel db=new DBModel())
+            {
+                db.TblEmployees.Add(emp);
+                db.SaveChanges();
+                return Json(new { success=true, message="Saved Successfully" },JsonRequestBehavior.AllowGet);
+            }
+
+        }
+
+
+
+
+
+
+
+    }
+}
